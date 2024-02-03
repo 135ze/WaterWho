658dadff396f96e0fc7a013179fae9680a420cc6
AnshV2 <misteransh@gmail.com>
2024-02-03 14:41:05 -0500

    fixed prod issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 import { unstable_noStore as noStore } from "next/cache";
 import Link from "next/link";
 
-import { CreatePost } from "~/app/_components/create-post";
 import { api } from "~/trpc/react";
 import styles from "./index.module.css";
 import { SignIn,  useUser } from "@clerk/nextjs";
