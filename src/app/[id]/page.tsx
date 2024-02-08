8958a6852b06b3c8ae0c5290fece7ad075899b52
Christina Li <christinayxli@gmail.com>
2024-02-07 22:39:29 -0500

    buttons, welcome back message

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -3,10 +3,9 @@
 import { unstable_noStore as noStore } from "next/cache";
 import Link from "next/link";
 import {Navbar} from "../components/navbar"
-
 import { api } from "~/trpc/react";
-import styles from "./index.module.css";
 import { SignIn,  useUser } from "@clerk/nextjs";
+import styles from "./Account.module.css";
 
 export default function Home() {
   noStore();
@@ -14,6 +13,15 @@ export default function Home() {
   return (
     <div>
         <Navbar></Navbar>
+        <div className={styles.accountContainer}>
+          <h1 className={styles.accountHeader}>Welcome back, John!</h1> {/* Add in user name later based on login*/}
+          <div className={styles.accountButtonsContainer}>
+            <button className={styles.accountButton}> Dashboard </button>
+            <button className={styles.accountButton}> Interviews </button>
+            <button className={styles.accountButton}> Rankings </button>
+            <button className={styles.accountButton}> My Applications </button>
+          </div>
+        </div>
     </div>
   );
 }
