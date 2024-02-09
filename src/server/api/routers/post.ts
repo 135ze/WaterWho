45402a93d784bbf08542ce3e9fb70fdadd7bd6a0
AnshV2 <misteransh@gmail.com>
2024-02-09 07:38:08 -0500

    added database schema

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,14 +1,15 @@
 import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
 import { clerkClient } from '@clerk/nextjs';
 
 export const postRouter = createTRPCRouter({
   hello: publicProcedure
     .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
+    .query(({ ctx, input }) => {
       return {
         greeting: `Hello ${input.text}`,
+        
       };
     }),
 });
