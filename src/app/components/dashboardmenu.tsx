ea117dba4791b2853a93cac8d395857e82e84d3f
Christina Li <christinayxli@gmail.com>
2024-02-09 23:58:11 -0500

    added some match cards with some fake datas!

--- src/app/components/dashboardmenu.tsx
+++ src/app/components/dashboardmenu.tsx
@@ -1,11 +1,21 @@
 import styles from "./Dashboard.module.css";
 import React, { useState } from "react";
-
+import { MatchCard } from "./matchCard";
 export function DashboardMenu() {
   const [activeButton, setActiveButton] = useState<string>("Matches");
   const handleButtonClick = (buttonName: string) => {
     setActiveButton(buttonName);
   };
+  const [matchData, setMatchData] = useState([
+    { id: 1, displayName: 'Mango', age: 21, program: "Computer Science", term: "2A" },
+    { id: 2, displayName: 'Orange', age: 20, program: "Software Engineering"},
+    { id: 3, displayName: 'Banana', age: 20, program: "Computer Science & Business Administration", term: "2B"},
+    { id: 4, displayName: 'Grape', age: 21, program: "I am a droput" , term: "2B"},
+    { id: 5, displayName: 'Strawberry', age: 21, program: "Wahoo" , term: "2B"},
+    { id: 5, displayName: 'LooooongName', age: 21, program: "Systems Design Engineering", term: "2B" },
+    /* These are just for testing purposes*/
+  ]);
+
   return (
     <div className={styles.dm}>
       <div className={styles.dmContainer}>
@@ -41,7 +51,19 @@ export function DashboardMenu() {
           )}
         </div>
       </div>
-      <div className={styles.dmBody}>{/*Match cards to be placed here*/}</div>
+      <div className={`${styles.dmBody} ${activeButton == "Matches" ? styles.dmMatchesGrid: ""}`}>
+        {activeButton === "Matches" &&
+        /* TO DO: future, may want to check for values existence before rendering for safety */
+          matchData.map((match) => (
+            <MatchCard
+              key={match.id}
+              id = {match.id}
+              displayName = {match.displayName}
+              age = {match.age}
+              program = {match.program}
+            />
+          ))}
+      </div>
     </div>
   );
 }
