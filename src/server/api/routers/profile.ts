7691821788a88c959ea4d778828e04658e0e84f3
Sophia Sun <53122458+sophiiasun@users.noreply.github.com>
2024-04-02 14:46:08 -0400

    Updated endpoints based on schema changes

--- src/server/api/routers/profile.ts
+++ src/server/api/routers/profile.ts
@@ -15,83 +15,101 @@ export const profileRouter = createTRPCRouter({
 
     upsertProfile: privateProcedure
     .input(z.object({
-      userID: z.number(), 
-      firstName: z.string(),
-      lastName: z.string(), 
-      projectedGraduationDate: z.date(),
-      mbti: MbtiEnum,
-      genderIdentity: z.string(),
-      sexuality: z.string(),
-      preferredGender: z.string(),
-      dateOfBirth: z.date(),
-      height: z.number(), 
-      nextTerm1: z.string(), 
-      nextTerm2: z.string(), 
-      nextTerm3: z.string(), 
-      cityOfResidence: z.string(),
-      provinceOfResidence: z.string(),
-      accountCreatedDate: z.date(),
+      UserID: z.number(), 
+      Name: z.string(),
+      DateOfBirth: z.string(),
+      Gender: z.string(),
+      SexualOrientation: z.string(),
+      Height: z.number(),
+      School: z.string(),
+      YearAndMajor: z.string(),
+      Location: z.string(),
+      CurrentStudyTerm: z.string(),
+      Mbti: MbtiEnum,
+      Tags: z.string(),
+      PhoneNumber: z.string(),
+      Discord: z.string(),
+      Instagram: z.string(),
+      PersonalDescription: z.string(),
+      RelationshipStyle: z.string(),
+      BenefitsAndCompensation: z.string(),
+      Interests: z.string(),
+      DealBreakers: z.string(),
+      DesiredTraits: z.string(),
+      AccountCreatedDate: z.date()
     }))
     .mutation(async ({ ctx, input}) => {
       const user = await ctx.db.userProfile.upsert ({
         where: {
-          userID: input.userID,
+          UserID: input.UserID,
         },
         update: {
-          userID: input.userID,
-          firstName: input.firstName,
-          lastName: input.lastName,
-          projectedGraduationDate: input.projectedGraduationDate,
-          mbti: input.mbti,
-          genderIdentity: input.genderIdentity,
-          sexuality: input.sexuality,
-          preferredGender: input.preferredGender,
-          dateOfBirth: input.dateOfBirth,
-          height: input.height, 
-          nextTerm1: input.nextTerm1,
-          nextTerm2: input.nextTerm2,
-          nextTerm3: input.nextTerm3,
-          cityOfResidence: input.cityOfResidence,
-          provinceOfResidence: input.provinceOfResidence,
-          accountCreatedDate: input.accountCreatedDate,
+          UserID: input.UserID, 
+          Name: input.Name,
+          DateOfBirth: input.DateOfBirth,
+          Gender: input.Gender,
+          SexualOrientation: input.SexualOrientation,
+          Height: input.Height,
+          School: input.School,
+          YearAndMajor: input.YearAndMajor,
+          Location: input.Location,
+          CurrentStudyTerm: input.CurrentStudyTerm,
+          Mbti: input.Mbti,
+          Tags: input.Tags,
+          PhoneNumber: input.PhoneNumber,
+          Discord: input.Discord,
+          Instagram: input.Instagram,
+          PersonalDescription: input.PersonalDescription,
+          RelationshipStyle: input.RelationshipStyle,
+          BenefitsAndCompensation: input.BenefitsAndCompensation,
+          Interests: input.Interests,
+          DealBreakers: input.DealBreakers,
+          DesiredTraits: input.DesiredTraits,
+          AccountCreatedDate: input.AccountCreatedDate
         },
         create: {
-          userID: input.userID,
-          firstName: input.firstName,
-          lastName: input.lastName,
-          projectedGraduationDate: input.projectedGraduationDate,
-          mbti: input.mbti,
-          genderIdentity: input.genderIdentity,
-          sexuality: input.sexuality,
-          preferredGender: input.preferredGender,
-          dateOfBirth: input.dateOfBirth,
-          height: input.height, 
-          nextTerm1: input.nextTerm1,
-          nextTerm2: input.nextTerm2,
-          nextTerm3: input.nextTerm3,
-          cityOfResidence: input.cityOfResidence,
-          provinceOfResidence: input.provinceOfResidence,
-          accountCreatedDate: input.accountCreatedDate,
+          UserID: input.UserID, 
+          Name: input.Name,
+          DateOfBirth: input.DateOfBirth,
+          Gender: input.Gender,
+          SexualOrientation: input.SexualOrientation,
+          Height: input.Height,
+          School: input.School,
+          YearAndMajor: input.YearAndMajor,
+          Location: input.Location,
+          CurrentStudyTerm: input.CurrentStudyTerm,
+          Mbti: input.Mbti,
+          Tags: input.Tags,
+          PhoneNumber: input.PhoneNumber,
+          Discord: input.Discord,
+          Instagram: input.Instagram,
+          PersonalDescription: input.PersonalDescription,
+          RelationshipStyle: input.RelationshipStyle,
+          BenefitsAndCompensation: input.BenefitsAndCompensation,
+          Interests: input.Interests,
+          DealBreakers: input.DealBreakers,
+          DesiredTraits: input.DesiredTraits,
+          AccountCreatedDate: input.AccountCreatedDate
         }
       })
       return user;
     }),
 
     getProfile: privateProcedure
-    .input(z.object({userID: z.number()}))
+    .input(z.object({UserID: z.number()}))
     .query(async ({ctx, input}) => {
       const ret = await ctx.db.userProfile.findFirstOrThrow ({ 
-        where: { userID: input.userID }
+        where: { UserID: input.UserID }
       })
       return ret;
     }),
 
     getMatches: privateProcedure
-    .input(z.object({userID: z.number()}))
+    .input(z.object({UserID: z.number()}))
     .query(async ({ctx, input}) => {
         const ret = await ctx.db.applications.findMany ({
-          where: { applicantID: input.userID,
-                    applicationStatus:  ApplicationStatusEnum.enum.Accepted
+          where: { applicantID: input.UserID,
+                  applicationStatus:  ApplicationStatusEnum.enum.Accepted
                   },
           select: {receiverID: true}
         })
