9766d674b3b3a5304e8394143ee5e47b6d8b127f
AnshV2 <misteransh@gmail.com>
2024-04-14 16:17:32 -0400

    schema changes, seperated enums, lots of database changes ask for new env file

---
+++ src/enums.js
@@ -0,0 +1,17 @@
+//const file to store our enum classes for front end use.
+//i'm adding comments this shit is so cool. so people will acc read this to get info.
+//would it be funny if i added like easter eggs in the comments.
+//this was all written by chwistina
+
+
+const mbti = ["INTJ", 'INTP', 'ENTJ', 'ENTP', 'INFJ', 'INFP', 'ENFJ', 'ENFP', 'ISTJ', 'ISFJ', 'ESTJ',
+ 'ESFJ', 'ISTP', 'ISFP', 'ESTP', 'ESFP']
+
+const relationshipstatus = ['None', 'Pending', 'Dating']
+
+const applicationstatus = ['None', 'Pending', 'Rejected', 'Accepted']
+
+const imagetype = ['PDF', 'PNG', 'OTHER']
+
+const socialtype = ['Instagram', 'Facebook', 'Github', 'Discord']
+
