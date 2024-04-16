3ccac99a62e17e92c5101cc035ec39b3a893e6d2
Jesse Huang <87463074+jeessh@users.noreply.github.com>
2024-04-16 18:27:34 -0400

    feat: added bar with progress props

---
+++ src/app/components/onboardprogress.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import styles from './OnboardProgress.module.scss'
+
+interface OnboardProgressProps {
+    currentPage: number;
+}
+
+const OnboardProgress = (props: OnboardProgressProps) => {
+    const {currentPage} = props;
+
+    let progress = {Basic_Info: "D9D9D9", Your_Posting: "D9D9D9", Complete: "D9D9D9"};
+    switch (currentPage) {
+        case 1:
+            progress = {
+                Basic_Info: "326096",
+                Your_Posting: "D9D9D9",
+                Complete: "D9D9D9"
+            }
+            break;
+        case 2:
+            progress = {
+                Basic_Info: "67A3EB",
+                Your_Posting: "326096",
+                Complete: "D9D9D9"
+            }
+            break;
+        case 3:
+            progress = {
+                Basic_Info: "67A3EB",
+                Your_Posting: "67A3EB",
+                Complete: "326096"
+            }
+            break;
+    }
+
+    const removeUnderscore = (str: string) => {
+        return str.replace(/_/g, " ");
+    }
+            
+    return (
+        <section className={styles.container}>
+            {
+                Object.entries(progress).map(([k, v], index) => {
+                    return (
+                        <div style={{backgroundColor: `#${v}`, color: `#${v}`, borderColor: `#${v}`}} className={styles.wrapper} key={index}>
+                            <p style={v === "D9D9D9" ? {color: "black"} : {color: "white"}} className={styles.text}>
+                                {removeUnderscore(k)}
+                            </p>
+                        </div>
+                    )
+            })}
+        </section>
+    )
+}
+
+export default OnboardProgress
