b1ffe8e568e6d81004b42e8a14e30236238fc485
Jesse Huang <87463074+jeessh@users.noreply.github.com>
2024-04-16 13:14:03 -0400

    feat: added components and styling + cute image

--- src/app/[fallback]/page.tsx
+++ src/app/[fallback]/page.tsx
@@ -1,6 +1,29 @@
-import React from 'react'
-
+import React from "react";
+import Image from "next/image";
+import errorImage from "../assets/sadpiplup.jpeg";
+import styles from "./fallback.module.scss";
+import { Navbar } from "../components/navbar";
 
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>
+  // ...
+
+  return (
+    <main className={styles.main}>
+      <Navbar />
+      <section className={styles.container}>
+        <div className={styles.textWrapper}>
+          <h1>Uh oh!</h1>
+          <h4>We couldn&apos;t find the page you were looking for :(</h4>
+          <p>lmao loser</p>
+        </div>
+        <div className={styles.imageWrapper}>
+          <Image
+            src={errorImage}
+            alt="404 - Page Not Found"
+            className={styles.image}
+          />
+        </div>
+      </section>
+    </main>
+  );
 }
