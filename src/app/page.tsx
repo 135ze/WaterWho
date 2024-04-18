63c4b70dcb6f3250791dba4a004a84b589c746c4
Jesse Huang <87463074+jeessh@users.noreply.github.com>
2024-04-18 17:05:13 -0400

    feat: application about and photos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,15 @@ import {Navbar} from "./components/navbar"
 import { api } from "~/trpc/react";
 import styles from "./index.module.scss";
 import { SignIn,  useUser } from "@clerk/nextjs";
+import OnboardingApplication from "./[onboarding-application]/application";
+import OnboardProgress from "./components/onboardprogress";
 
 export default function Home() {
   noStore();
 
   return (
     <div>
-      this is the landing page.
+      <OnboardingApplication/>
     </div>
   );
 }
