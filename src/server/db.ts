26131aa89853ac08d4bb656c06a42cb6d751844c
AnshV2 <misteransh@gmail.com>
2024-04-23 09:49:15 -0400

    coded middleware file and added some other stuff

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,21 +1,27 @@
-import { Client } from "@planetscale/database";
-import { PrismaPlanetScale } from "@prisma/adapter-planetscale";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
+import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { createClient } from '@libsql/client'
 
-import { env } from "~/env.js";
+import { env } from "~/env";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const client = new Client({ url: env.DATABASE_URL });
+const libsql = createClient({
+  url: `${process.env.DATABASE_URL}`,
+  authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+})
+const adapter = new PrismaLibSQL(libsql)
+
 
 export const db =
   globalForPrisma.prisma ??
-  new PrismaClient({
+  new PrismaClient({ adapter,
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-    adapter: new PrismaPlanetScale(client),
   });
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+
+
